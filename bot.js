fa63af26192f3ad4896f0b8986093d8979d848d8
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-02-15 21:29:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('ready', () => {
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(` اعتني بها جيدا ياصاحب النصيب فانا مازلت احبها `,"http://twitch.tv/Death Shop")
-client.user.setStatus("dnd")
+client.user.setStatus("Online")
 });
 
 
