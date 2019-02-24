718920bc3b2347722cdbd444dbc6ddda2911d6fb
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-02-24 14:28:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('message', message => {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**Status You  ${argresult}**`)
   } else 
-  if (message.content.startsWith(adminprefix + 'ss')) {
+  if (message.content.startsWith(adminprefix + 'st')) {
     client.user.setGame(argresult, "https://www.twitch.tv/M3roof");
       message.channel.send(`**Status You ${argresult} **`)
 }
