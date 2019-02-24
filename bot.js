e446a04e37c519498013442b3850e959be7607e0
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-02-24 12:43:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Retir'ed ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` شــقــفــه 🤣 `,"http://twitch.tv/Death Shop")
+client.user.setGame(` WnAsH 🤣 `,"http://twitch.tv/Death Shop")
 client.user.setStatus("Online")
 });
 
